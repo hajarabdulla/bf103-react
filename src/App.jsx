0875825ba77c914e36608761d331cb9c0367aa59
hajarabdullayeva <hadjar.abdullayeva@gmail.com>
2023-11-17 09:56:29 +0400

    virtual dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const userData = [
+  {
+    id: uuidv4(),
+    name: "Hajar",
+    university: "asoiu",
+  },
+  {
+    id: uuidv4(),
+    name: "Vafa",
+    university: "bsu",
+  },
+];
 
 const App = () => {
-  return <div>App</div>;
+  const [data, setData] = useState(userData);
+
+  const handleDelete = (id) => {
+    console.log(id);
+    const newData = data.filter((item) => id != item.id);
+    setData(newData);
+  };
+
+  return (
+    <ul>
+      {data.map((user, index) => (
+        <li key={user.id}>
+          <h3>{user.id}</h3>
+          <p>{user.name}</p>
+          <p>{user.university}</p>
+          <button onClick={() => handleDelete(user.id)}>Delete</button>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default App;
