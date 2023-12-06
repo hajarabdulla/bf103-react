2637cc3fcbed64c38cd84e74213a3917bfc3824b
hajarabdullayeva <hadjar.abdullayeva@gmail.com>
2023-12-06 10:37:56 +0400

    redux

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,15 @@ import {
   increaseCounter,
 } from "./redux/features/counterSlice";
 import Form from "./components/Form";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import "./styles.css";
+
+// import required modules
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper/modules";
+import SliderImage1 from "./assets/images/aa.avif";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -12,6 +21,36 @@ const App = () => {
 
   return (
     <div>
+      {/* <Helmet>
+        <meta charSet="utf-8" />
+        <title>Home</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet> */}
+
+      <Swiper
+        cssMode={true}
+        navigation={true}
+        pagination={true}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={SliderImage1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+
       <h1>counter: {counter} </h1>
       <button onClick={() => dispatch(increaseCounter())}>Increase</button>
       <button onClick={() => dispatch(descreaseCounter())}>Descrease</button>
